1b1a063e1112f30373293b805335ef81731a0600
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-10-05 11:11:40 +0300

    nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="relative h-screen">
     <slot></slot>
+    <div class="fixed top-2 right-2 flex gap-2 text-gray-400">
+      <NuxtLink to="/emotionally">Emotionally</NuxtLink>
+      <NuxtLink to="/tuju">Tuju</NuxtLink>
+      <NuxtLink to="/ketas">Ketas</NuxtLink>
+    </div>
   </div>
 </template>
