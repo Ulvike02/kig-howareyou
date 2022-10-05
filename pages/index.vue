7da66997057915e9246a094240b951be34620592
karmelp <101389533+karmelp@users.noreply.github.com>
2022-10-05 10:31:21 +0300

    mummu

--- pages/index.vue
+++ pages/index.vue
@@ -49,6 +49,39 @@ const options2 = [
   },
 ];
 
+const options3 = [
+  {
+    title: "Happy",
+    fill: "#2F4E3E",
+    icon: "https://api.iconify.design/iconoir:emoji-look-bottom.svg",
+  },
+  {
+    title: "Sad",
+    fill: "#556D73",
+    icon: "https://api.iconify.design/iconoir:emoji-look-bottom.svg",
+  },
+  {
+    title: "Angry",
+    fill: "#252C23",
+    icon: "https://api.iconify.design/iconoir:emoji-look-bottom.svg",
+  },
+  {
+    title: "Anxious",
+    fill: "#3A4240",
+    icon: "https://api.iconify.design/iconoir:emoji-look-bottom.svg",
+  },
+  {
+    title: "Mental block",
+    fill: "#556D73",
+    icon: "https://api.iconify.design/iconoir:emoji-look-bottom.svg",
+  },
+  {
+    title: "Confused",
+    fill: "#3A4240",
+    icon: "https://api.iconify.design/iconoir:emoji-look-bottom.svg",
+  },
+];
+
 const selectedSector = $ref();
 const onSelect = (sector) => (selectedSector = sector);
 </script>
@@ -58,7 +91,7 @@ const onSelect = (sector) => (selectedSector = sector);
     class="flex items-center justify-center border-4 border-red-500 text-red-400 h-full"
   >
     <div>
-      <ColorWheel @select="onSelect" :options="options1" />
+      <MummuWheel @select="onSelect" :options="options3" />
       <!-- <EmojiWheel @select="onSelect" :options="options1" /> -->
       <!-- <EmojiWheel @select="onSelect" :options="options2" /> -->
       You selected {{ selectedSector?.title }}
